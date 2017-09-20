684d6b7833657910a8d16e71ee5747e1fca48113
Jerry W <jerrywiltse@gmail.com>
2017-09-19 23:30:14 -0400

    changed refs to testing

--- conanfile.py
+++ conanfile.py
@@ -8,11 +8,11 @@ class BoostArrayConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["array"]
-    requires =  "Boost.Config/1.65.1@bincrafters/stable", \
-                      "Boost.Assert/1.65.1@bincrafters/stable", \
-                      "Boost.Core/1.65.1@bincrafters/stable", \
-                      "Boost.Static_Assert/1.65.1@bincrafters/stable", \
-                      "Boost.Throw_Exception/1.65.1@bincrafters/stable"
+    requires =  "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Throw_Exception/1.65.1@bincrafters/testing"
                       
     def source(self):
         boostorg_github = "https://github.com/boostorg"
