d42fcf161c12fb15b3c9eacd839114cb0fa93e0e
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 21:59:07 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostArrayConan(base.BoostBaseConan):
     name = "boost_array"
@@ -18,5 +18,3 @@ class BoostArrayConan(base.BoostBaseConan):
         "boost_static_assert",
         "boost_throw_exception"
     ]
-
-
